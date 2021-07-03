32997820b000a919173efa35dad449384ea360c7
DeBejara <74940042+DeBejara@users.noreply.github.com>
2021-07-02 20:49:00 -0500

    Update Main.java

--- src/main/java/ec/edu/espol/proyectopoo/Main.java
+++ src/main/java/ec/edu/espol/proyectopoo/Main.java
@@ -16,6 +16,38 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("***MENÚ DE OPCIONES***");
+            System.out.println("\t1. Vendedor");
+            System.out.println("\t2. Comprador");
+            System.out.println("\t3. Salir");
+            System.out.println("Una vez escrita la opción, pulse la tecla Enter");
+
+            opcion = scanner.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                    System.out.println("Usted ha elegido la opción Vendedor");
+                    System.out.println("\t1. Registrar un nuevo vendedor");
+                    System.out.println("\t2. Ingresar un nuevo vendedor");
+                    System.out.println("\t3. Aceptar oferta");
+                    System.out.println("\t4. Regresar");                    
+                    break;
+                case 2:
+                    System.out.println("Usted ha elegido la opción Comprador");
+                    System.out.println("\t1. Registrar un nuevo comprador");
+                    System.out.println("\t2. Ofertar por un vehículo");                    
+                    break;
+                case 3:
+                    System.out.println("Muchas gracias por usar nuestros servicios.");
+                    break;
+                default:
+                    System.out.println("No ha elegido una opción válida. Ingrese una opción válida.");
+                    break;
+            }
+        } while (opcion != 3);        
     }
     
 }
