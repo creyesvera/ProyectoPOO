d611a251f919619f4db3e585375796753c66067c
creyesvera <86809908+creyesvera@users.noreply.github.com>
2021-07-08 21:59:31 -0500

    Update Comprador.java

--- src/main/java/ec/edu/espol/model/Comprador.java
+++ src/main/java/ec/edu/espol/model/Comprador.java
@@ -65,7 +65,7 @@ public class Comprador extends Usuario{
     }
         
     
-    public static void comprar(Vehiculo v, String nomfile){ //ofertas.txt
+    public void comprar(Vehiculo v, String nomfile){ //ofertas.txt
         int id_oferta = Util.nextID(nomfile);
         double precio = Double.parseDouble(JOptionPane.showInputDialog(null,"Por favor ingrese el precio de su oferta usando el "." como separador decimal:", "CompraVende", JOptionPane.INFORMATION_MESSAGE); 
         Oferta new_oferta = Oferta(id_oferta, this.id, v.getId, precio);
