51b1d67f1f717d24f257ba02278c400541ed5a93
creyesvera <86809908+creyesvera@users.noreply.github.com>
2021-07-09 02:13:13 -0500

    Update Vendedor.java

--- src/main/java/ec/edu/espol/model/Vendedor.java
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -31,36 +31,6 @@ public class Vendedor extends Usuario{
     }
 
     @Override
-    public int hashCode() {
-        int hash = 3;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        //comparar atributos que bienen de la clase padre
-        Usuario este_u = this;
-        Usuario other_u = (Usuario) obj;
-        if (!Objects.equals(este_u, other_u))
-            return false;
-        //comparar atributo exclusivo de esta clase
-        Vendedor other = (Vendedor) other_u;
-        if (!Objects.equals(this.vehiculos, other.vehiculos)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
     public String toString() {
         return "Vendedor{" +"id=" + id + ", nombres=" + nombres + ", apellidos=" + apellidos + ", correo_elec=" + correo_elec + ", organizacion=" + organizacion + ", clave=" + clave + "vehiculos=" + vehiculos + '}';
     }
