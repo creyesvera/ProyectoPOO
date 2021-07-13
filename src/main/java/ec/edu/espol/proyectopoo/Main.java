6e244f20d1a846927c028756b526df4ec20af1d7
creyesvera <86809908+creyesvera@users.noreply.github.com>
2021-07-13 18:16:44 -0500

    Update Main.java

--- src/main/java/ec/edu/espol/proyectopoo/Main.java
+++ src/main/java/ec/edu/espol/proyectopoo/Main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
         int opcionSeleccionada;
         do{
             
